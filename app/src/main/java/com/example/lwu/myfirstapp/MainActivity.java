784ee3585c4d4ee8d3b79de1ecd1441d2d41ba6c
leonnnwu <wuliang0306@gmail.com>
2016-11-21 19:34:56 -0800

    Activity life cycle.

--- app/src/main/java/com/example/lwu/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/lwu/myfirstapp/MainActivity.java
@@ -9,10 +9,13 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.lwu.myfirstapp.MESSAGE";
+    public final String STATE_SCORE = "playerScore";
+    public final String STATE_LEVEL = "playerLevel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 
@@ -22,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        //Save the user's current game state
+        savedInstanceState.putInt(STATE_SCORE, 1);
+        savedInstanceState.putInt(STATE_LEVEL, 10);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.getInt(STATE_SCORE);
     }
 }
